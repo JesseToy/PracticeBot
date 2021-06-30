0fb8d3fdf9e6ed38f1411a7e08a388279577ee11
Jesse T <39315839+JesseToy@users.noreply.github.com>
2021-06-30 18:18:13 +0000

    Added: Twitch link, Stream Elements link

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$StreamTime'):
+  if message.content.startswith('$Time'):
     await message.channel.send('9:30 PM EST')
+
+  if message.content.startswith('$Stream'):
+    await message.channel.send('https://www.twitch.tv/toxicflexx1')
+
+  if message.content.startswith('$Tip'):
+    await message.channel.send('https://streamelements.com/toxicflexx1/tip')
 
 client.run(os.getenv('TOKEN'))
